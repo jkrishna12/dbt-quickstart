31f93307fdd5e8f75f76a03a7efcf8d14f74ceb6
krishna.chintamreddy@snapanalytics.co.uk <krishna.chintamreddy@snapanalytics.co.uk>
2026-04-23 13:46:47 +0000

    change materialisation strategy at folder level in dbt_project.yml. And added sub folder jaffle_shop to staging

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,14 +1,21 @@
 with customers as (
-    select * from {{ ref('stg_jaffle_shop__customers') }}
+    
+    select
+        *
+    from
+        {{ ref('stg_jaffle_shop__customers') }}
 ),
 orders as (
-    select * from {{ ref('stg_jaffle_shop__orders') }}
+    
+    select
+        *
+    from
+        {{ ref('stg_jaffle_shop__orders') }}
 ),
 customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
