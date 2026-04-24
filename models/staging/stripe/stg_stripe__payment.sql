7305853b8fd349001a3ae004d657e926ee076c56
krishna.chintamreddy@snapanalytics.co.uk <krishna.chintamreddy@snapanalytics.co.uk>
2026-04-24 14:09:29 +0000

    add freshness for the stripe payments

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -11,8 +11,8 @@ renamed as (
         orderid as order_id,
         paymentmethod as payment_method,
         status as payment_status,
-        amount as payment_amount,
-        created as payment_created,
+        amount / 100 as payment_amount,
+        _batched_at as payment_created,
 
     from source
 
